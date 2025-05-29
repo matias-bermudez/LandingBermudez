eaa0fce3ee213d41d8aec4103f115e3ddf8f1b59
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-05-29 11:57:15 -0300

    agrego componentes Navbar y CartWidget

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,15 @@
+import logoCarrito from '../assets/carrito.png'
+import './CartWidget.css'
+
+function CartWidget () {
+
+    const contador = 2;
+    return (
+        <div className="carrito">
+            <button><a href=""><img src={logoCarrito} alt="Imagen de carrito de compras"/></a></button>
+            <p>{contador}</p>
+        </div>
+    );
+}
+
+export default CartWidget
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import CartWidget from './CartWidget'
+import './Navbar.css'
+import logo from '../assets/logo-logi.png'
+
+function Navbar () {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="#">Auriculares</a></li>
+                <li><a href="#">Teclados</a></li>
+                <li><a href="#"><img src={logo} alt="Logo Logitech"/></a></li>
+                <li><a href="#">Ratones</a></li>
+                <li><a href="#">Camaras Web</a></li>
+            </ul>
+            <CartWidget />
+        </nav>
+    );
+}
+
+export default Navbar
