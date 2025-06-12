0710bc3039ce5db73e7df36d0f959f36c72c514c
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-06-12 12:51:33 -0300

    Agrego productos y enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import Navbar from './components/Navbar'
 import ItemListContainer from './components/ItemListContainer'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <ItemListContainer 
-        titulo={'¡Bienvenido a la tienda Logitech!'}
-        slogan={'Lo mejor para que tu experiencia tecnologica sea superior'} 
-      />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <ItemListContainer
+              titulo={'¡Bienvenido a la tienda Logitech!'}
+              slogan={'Lo mejor para que tu experiencia tecnológica sea superior'}
+            />
+          } />
+          <Route path="/categoria/:type" element={
+            <ItemListContainer
+              titulo={'Productos por categoría'}
+              slogan={'Encontrá lo mejor de cada tipo'}
+            />
+          } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
