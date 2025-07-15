07e312e4a2a1d1eec9d8a0d63253c13b0051af8d
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-07-14 22:45:13 -0300

    encaro vista de carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,25 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import CartItem from "../../components/CartItem/CartItem"
+import './Cart.css' 
 
 function Cart () {
     const { cart } = useContext(CartContext)
-    const productos = cart
-    return (
-        cart.map(prod => {
-            <CartItem producto={prod} />
-        })
-    )
+    console.log("contenido del carrito", cart)
+    if(cart) {
+        return (
+            <section className="seccion-carrito">
+                {   
+                    cart.map(prod => (
+                        <CartItem key={prod.id} producto={prod} />
+                    ))
+                }
+            </section>
+        )
+    } else {
+        console.log("no ai nad")
+    }
+    
 }
 
 export default Cart
