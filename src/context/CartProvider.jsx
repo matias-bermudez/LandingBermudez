b15492e0fd9088fa52d8987b4685146eba494510
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-07-15 20:25:23 -0300

    objetos se agregan y salen del carrito bien. falta ver que cuando se da a agregar al carrito no se agregue el objeto sino que se sume uno a la cant

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -18,8 +18,44 @@ function CartProvider ( {children} ) {
         }
     }
 
+    const eraseItem = ( obj ) => {
+        setCart(cart.filter(prod => prod.id !== obj.id))
+    }
+
+    const subItem = ( obj ) => {
+        if(obj.quantity <= 1) {
+            eraseItem(obj)
+        } else {
+            let indice = cart.findIndex( prod => prod.id == obj.id)
+            if (indice != -1) {
+                const cartUpdate = [...cart]
+                const prod = { ...cartUpdate[indice] }
+                prod.quantity -= 1
+                if(prod.quantity <= 1) {
+                    eraseItem(obj)
+                } else {
+                    cartUpdate[indice] = prod
+                    setCart(cartUpdate)
+                }
+            }
+        }
+    }
+
+    const addItem = ( obj ) => {
+        if(obj.quantity < 99) {
+            let indice = cart.findIndex( prod => prod.id == obj.id)
+            if (indice != -1) {
+                const cartUpdate = [...cart]
+                const prod = { ...cartUpdate[indice] }
+                prod.quantity += 1
+                cartUpdate[indice] = prod
+                setCart(cartUpdate)
+            }
+        }
+    }
+
     return (
-        <CartContext.Provider value={ {cart, addToCart, getQuantity} }>
+        <CartContext.Provider value={ {cart, addToCart, getQuantity, eraseItem, subItem, addItem} }>
             {children}
         </CartContext.Provider>
     )
