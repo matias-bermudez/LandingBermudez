9e940c053b96745288f6ba80d9bbde510dc95c52
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-07-18 20:41:25 -0300

    mejoro dise;o de detail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,24 +5,26 @@ function ItemDetail ( { producto } ) {
 
     return (
         <section key={producto.id} className="detalle-producto">
-            <div className="fotos-info">
-                <img src={producto.imgDetail} alt={producto.name} />
-                <div className="texto-producto">
-                    <h2>{producto.name}</h2>
-                    <div className="compra">
-                        <h2>U$S {producto.price}</h2>
-                        <p> | </p>
-                        <h2>Hasta 10 cuotas sin recargo</h2>
-                        <p> | </p>
-                        <ItemCount item={producto}/>
-                    </div>
-                </div>
+            <div className="informacion-producto">
+                    <h1>{producto.name}</h1>
+                    <h2>U$D {producto.price}</h2>
             </div>
-            <h3>Descripción</h3>
-            <p>{producto.description}</p>
+            <div className="descripcion">
+                <h3>Descripción</h3>
+                <p>{producto.description}</p>
+            </div>
+            <div className="agregar">
+                    <ItemCount item={producto} />
+            </div>
+            
+            <div className="fotos-individuales">
+                <img src={producto.img1} alt={producto.name} />
+                <img src={producto.img2} alt={producto.name} />
+            </div>
+                        <img src={producto.imgDetail} alt={producto.name} className="foto-principal"/>
+
         </section>
     )
 }
-
 
 export default ItemDetail
